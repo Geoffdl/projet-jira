4e4a7e671afcbbf364373836b18493ec3d0df40d
Geoffdl <geoffroydelorme@gmail.com>
2025-06-24 11:20:43 +0200

    init

--- src/app/shared/nav/nav.ts
+++ src/app/shared/nav/nav.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
   <div class="du-navbar hover:du-glass bg-primary text-primary-content">
-    <button class="du-btn du-btn-ghost text-xl">Angular App</button>
+    <button class="du-btn du-btn-ghost text-xl">JiraLike</button>
 </div>
   `,
   styles: ``,
